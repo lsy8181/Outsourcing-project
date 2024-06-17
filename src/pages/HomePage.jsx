31ee13578cdcd24cd58eb639defbd46cf6016762
butterbeetle <butterbeetle@naver.com>
2024-06-17 20:35:55 +0900

    Bugfix: tailwindcss 설치 후 버그 수정

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div className="bg-red-200">HomePage</div>;
+}
+
+export default HomePage;
---
+++ src/routes/router.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from 'react-router-dom';
+import EditPage from '../pages/EditPage';
+import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+import MyPage from '../pages/MyPage';
+import ReadPage from '../pages/ReadPage';
+import SignUpPage from '../pages/SignUpPage';
+import WritePage from '../pages/WritePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+  {
+    path: '/feeds/:feedId',
+    element: <ReadPage />
+  },
+  {
+    path: '/write',
+    element: <WritePage />
+  },
+  {
+    path: '/edit/:feedId',
+    element: <EditPage />
+  },
+  {
+    path: '/sign-up',
+    element: <SignUpPage />
+  },
+  {
+    path: '/log-in',
+    element: <LoginPage />
+  },
+  {
+    path: '/my-page/:userId',
+    element: <MyPage />
+  }
+]);
+
+export default router;
