0081140d04cc8e655e13373308d73349e879708b
butterbeetle <butterbeetle@naver.com>
2024-06-18 10:52:28 +0900

    Update: 클릭으로 마커 찍고 주소 가져오는 기능

--- src/pages/WritePage.jsx
+++ src/pages/WritePage.jsx
@@ -1,44 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDaumPostcodePopup } from 'react-daum-postcode';
+import useDidMountEffect from '../hooks/useDidMountEffect';
 
 const { kakao } = window;
-
-const themeObj = {
-  //bgColor: "", //바탕 배경색
-  searchBgColor: '#0B65C8', //검색창 배경색
-  //contentBgColor: "", //본문 배경색(검색결과,결과없음,첫화면,검색서제스트)
-  //pageBgColor: "", //페이지 배경색
-  //textColor: "", //기본 글자색
-  queryTextColor: '#FFFFFF' //검색창 글자색
-  //postcodeTextColor: "", //우편번호 글자색
-  //emphTextColor: "", //강조 글자색
-  //outlineColor: "", //테두리
-};
 
 function WritePage() {
   const [map, setMap] = useState(null);
   const [marker, setMarker] = useState(null);
+  const inputRef = useRef(null);
+
+  const geocoder = new kakao.maps.services.Geocoder();
   const open = useDaumPostcodePopup('https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js');
 
   // 카카오맵 API
   useEffect(() => {
-    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    const container = document.getElementById('map');
 
     const options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3 //지도의 레벨(확대, 축소 정도)
+      center: new kakao.maps.LatLng(37.5023270151927, 127.044444694599),
+      level: 3
     };
 
-    setMap(new kakao.maps.Map(container, options)); //지도 생성 및 객체 리턴
+    setMap(new kakao.maps.Map(container, options));
     setMarker(new kakao.maps.Marker());
   }, []);
 
   const completeHandler = (data) => {
     let fullAddress = data.address;
     let extraAddress = '';
-
-    const geocoder = new kakao.maps.services.Geocoder();
 
     if (data.addressType === 'R') {
       if (data.bname !== '') {
@@ -50,24 +39,22 @@ function WritePage() {
       fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
     }
 
+    inputRef.current.value = fullAddress;
+
     geocoder.addressSearch(fullAddress, function (result, status) {
-      // 정상적으로 검색이 완료됐으면
       if (status === kakao.maps.services.Status.OK) {
-        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+        // console.log(coords);
 
-        // 결과값으로 받은 위치를 마커로 표시합니다
-        var marker = new kakao.maps.Marker({
-          map: map,
-          position: coords
-        });
+        marker.setMap(null);
+        marker.setPosition(coords);
+        marker.setMap(map);
 
-        // 인포윈도우로 장소에 대한 설명을 표시합니다
-        var infowindow = new kakao.maps.InfoWindow({
-          content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
-        });
-        infowindow.open(map, marker);
+        // const infowindow = new kakao.maps.InfoWindow({
+        //   content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
+        // });
+        // infowindow.open(map, marker);
 
-        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
         map.setCenter(coords);
       }
     });
@@ -77,14 +64,58 @@ function WritePage() {
     open({ onComplete: completeHandler });
   };
 
+  useDidMountEffect(() => {
+    kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+      const coords = mouseEvent.latLng;
+      // console.log(coords);
+
+      geocoder.coord2Address(coords.getLng(), coords.getLat(), (result, status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          inputRef.current.value = result[0].road_address
+            ? result[0].road_address.address_name
+            : inputRef.current.value;
+        }
+
+        marker.setMap(null);
+        marker.setPosition(coords);
+        marker.setMap(map);
+      });
+    });
+  }, [map, marker]);
+
   return (
     <main>
-      <div className="max-w-[1440px] bg-red-200 mx-auto flex flex-col items-center justify-center p-4">
+      <div className="max-w-[1440px] bg-red-200 mx-auto flex flex-col items-center justify-center p-4 gap-6">
         <h1>WritePage</h1>
-        <div className="max-w-[1000px] w-full aspect-video mx-auto" id="map" />
-        <div className="">
-          <button onClick={onClickHandler}>버튼튼</button>
+        <div className="max-w-[500px] w-full flex border border-gray-200 divide-x-2 divide-solid">
+          <div className="relative w-full flex-1">
+            <input
+              ref={inputRef}
+              id={'aa'}
+              className="p-6 pb-px w-full text-base appearance-none outline-none
+                     text-[#0a0426] line-clamp-1
+                     hover:shadow-md peer select-none"
+              type="text"
+              placeholder=""
+              readOnly
+            />
+            <label
+              htmlFor="aa"
+              className="absolute top-4 left-6 text-base select-none text-[#a1a1aa] cursor-text
+                     duration-150 transform
+                     origin-[0]
+                     -translate-y-3 scale-75
+                     peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0
+                     peer-focus:scale-75 peer-focus:-translate-y-3"
+            >
+              선택된 주소
+            </label>
+          </div>
+          <button className="w-fit text-sm" onClick={onClickHandler}>
+            주소찾기
+          </button>
         </div>
+        <div className="border border-blue-600 max-w-[800px] w-full aspect-video mx-auto" id="map" />
       </div>
     </main>
   );
