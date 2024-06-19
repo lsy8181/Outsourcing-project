b04cd5a42c11c807515769961c61a8927506993e
hyunjeongkwak <lucyruby0457@gmail.com>
2024-06-19 11:24:23 +0900

    .

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_PROJECT_URL = 'https://xxeqrlcareyhdjuuyipu.supabase.co';
-const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4ZXFybGNhcmV5aGRqdXV5aXB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg3MTAzODQsImV4cCI6MjAzNDI4NjM4NH0.34tlyAXBh4YaTotFaCmri56J8GANT9k2Nf0ikJOLMcc';
+const SUPABASE_PROJECT_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_URL;
 
 const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
 
