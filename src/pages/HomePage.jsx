78317c9ce5949a08a7bc8f969cf9212bc23cbada
hj7321 <oboki2001@naver.com>
2024-06-19 11:24:21 +0900

    update: 데이터 가져와서 MainMap, PlaceList 컴포넌트에 prop으로 전달

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,29 @@
 import MainMap from '../components/MainMap';
 import PlaceList from '../components/PlaceList';
+import { useQuery } from '@tanstack/react-query';
+import api from '../api/api';
 
 const HomePage = () => {
+  const {
+    isLoading,
+    isError,
+    data: places
+  } = useQuery({
+    queryKey: ['places'],
+    queryFn: () => api.post.getPosts()
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {isError.message}</div>;
+
   return (
     <>
-      {/* 지도 화면 컴포넌트 */}
-      <MainMap />
-      {/* 추천 장소 리스트 컴포넌트 */}
-      <PlaceList />
+      {places && (
+        <>
+          <MainMap places={places} />
+          <PlaceList places={places} />
+        </>
+      )}
     </>
   );
 };
