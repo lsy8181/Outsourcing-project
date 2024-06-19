d8f7cca2e37b788bd7b4d87b230cbf8787517fbf
hj7321 <oboki2001@naver.com>
2024-06-19 17:31:51 +0900

    update: 각 장소에 맞는 정적 지도 생성 및 마커 생성 완료

--- src/components/PlaceList.jsx
+++ src/components/PlaceList.jsx
@@ -1,8 +1,14 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
+import { Link } from 'react-router-dom';
+
+const { kakao } = window;
 
 const PlaceList = ({ places }) => {
+  const [staticMapObj, setStaticMapObj] = useState(null); // 정적 지도 객체
+  const staticMapRef = useRef([]); // 정적 지도를 렌더링할 DOM 요소
+
   // const {
   //   data: places,
   //   hasNextPage,
@@ -27,20 +33,44 @@ const PlaceList = ({ places }) => {
   //   }
   // });
 
+  useEffect(() => {
+    staticMapRef.current.forEach((ref) => {
+      console.log(ref);
+      places.forEach((place) => {
+        if (ref.id == place.post_id) {
+          const container = ref;
+          // 정적 지도와 마커 생성
+          const location = new kakao.maps.LatLng(place.lat, place.lon);
+          const marker = {
+            position: location
+          };
+          const options = {
+            center: location,
+            level: 3,
+            marker: marker
+          };
+          const staticMap = new kakao.maps.StaticMap(container, options);
+          setStaticMapObj(staticMap);
+        }
+      });
+    });
+  }, [places]);
+
   return (
-    <section className="border border-blue-300 mt-5" style={{ width: '1300px' }}>
+    <section className="border border-blue-300 mt-5" style={{ width: '1400px' }}>
       <ul className="grid grid-cols-4 gap-6">
-        {/* 나중에 li 태그를 Link 태그로 변경 or li 태그에 navigate() 함수 사용 */}
-        {places.map((obj) => (
-          <li key={obj.post_id} className="border border-gray-300 p-2">
-            <div className="border" style={{ width: '100%', height: '150px' }}>
-              {/* {obj.img} */}
-              지도 사진
-            </div>
+        {places.map((obj, index) => (
+          <Link to={`/detail/${obj.post_id}`} key={obj.post_id} className="border border-gray-300 p-2">
+            <div
+              ref={(e) => (staticMapRef.current[index] = e)}
+              id={obj.post_id}
+              className="border"
+              style={{ width: '100%', height: '200px' }}
+            ></div>
             <p className="font-semibold text-lg">📍{obj.title}</p>
             <p>⭐{obj.star}</p>
             <p className="text-sm">🗒️{obj.address}</p>
-          </li>
+          </Link>
         ))}
       </ul>
     </section>
