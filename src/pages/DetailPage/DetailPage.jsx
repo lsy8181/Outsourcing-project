3a215d28e719c4f8c94bbe8b3f79476a68a739b8
butterbeetle <butterbeetle@naver.com>
2024-06-20 19:59:28 +0900

    Fix: 디테일페이지 지도 정적으로 변경

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -30,25 +30,22 @@ const Detail = () => {
   };
 
   useEffect(() => {
-    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    const options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(data[0].lat, data[0].lon), //지도의 중심좌표.
-      level: 3 //지도의 레벨(확대, 축소 정도)
-    };
-
-    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스//지도 생성 및 객체 리턴
 
     // 마커가 표시될 위치입니다
     const markerPosition = new kakao.maps.LatLng(data[0].lat, data[0].lon);
 
     // 마커를 생성합니다
-    const marker = new kakao.maps.Marker({
+    const marker = {
       position: markerPosition
-    });
-
-    // 마커가 지도 위에 표시되도록 설정합니다
-    marker.setMap(map);
+    };
+    const options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(data[0].lat, data[0].lon), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+      marker
+    };
+    const staticMap = new kakao.maps.StaticMap(container, options);
   }, []);
 
   return (
