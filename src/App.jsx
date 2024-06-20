7c05d6cac99b5b0be5bfeb3eeddb38c34a1082da
hj7321 <oboki2001@naver.com>
2024-06-20 14:12:29 +0900

    script: tanstack query devtools 설치

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import ModalContextProvider from './hooks/useModal';
 import ToastProvider from './hooks/useToast';
 import QueryProvider from './query/QueryProvider';
 import router from './routes/router';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
     <QueryProvider>
+      {/* <ReactQueryDevtools initialIsOpen={false}> */}
       <ModalContextProvider>
         <ToastProvider>
           <RouterProvider router={router} />
         </ToastProvider>
       </ModalContextProvider>
+      {/* </ReactQueryDevtools> */}
     </QueryProvider>
   );
 }
